4cf3b08810a700bf4224d66368b5f0e01d4f2dce
kazueyo <102661883+kazueyo@users.noreply.github.com>
2022-06-09 21:01:38 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-//logoの表示
-$(window).on('load',function(){
-  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
-});
+//画像をクリックしたら現れる画面の設定  
+$(".gallery-list").modaal({
+    fullscreen:'true', //フルスクリーンモードにする
+    before_open:function(){// モーダルが開く前に行う動作
+      $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+    },
+    after_close:function(){// モーダルが閉じた後に行う動作
+      $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+    }
+  });
